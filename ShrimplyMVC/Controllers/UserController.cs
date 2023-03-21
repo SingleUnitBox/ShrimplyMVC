92747f7af652cca08e85c359d1e840eca5c651fc
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-03-21 22:20:16 +0000

    add IdentityUser functionality for SuperAdmin

---
+++ ShrimplyMVC/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShrimplyMVC.Models;
+using ShrimplyMVC.Repositories;
+
+namespace ShrimplyMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = new UserIndexViewModel();
+            var users = await _userRepository.GetAllAsync();
+            if (users != null)
+            {
+                model.Users = users;
+                return View(model);
+            }    
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(AddUser addUser)
+        {
+            var user = new IdentityUser
+            {
+                UserName = addUser.Username,
+                Email = addUser.Email,
+            };
+            var roles = new List<string> { "User" };
+            if (addUser.AdminCheckbox)
+            {
+                roles.Add("Admin");
+            }
+            var result = await _userRepository.AddAsync(user, addUser.Password, roles);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ShrimplyMVC/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ShrimplyMVC.Models;
+using ShrimplyMVC.Models.Domain;
+
+namespace ShrimplyMVC.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<IdentityUser>> GetAllAsync();
+        Task<bool> AddAsync(IdentityUser identityUser, string password, List<string> roles);
+    }
+}
